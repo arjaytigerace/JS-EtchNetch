f72b4efdcaa1eb5949a1a00e8ec3b837368c3a1d
Raeniel Jan Cornelio <raeniel2098@gmail.com>
2023-11-13 23:06:38 +0800

    Finished up with a change grid button to reset

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function createGrid(size) {
   // Accessing the container div from the DOM
   const container = document.getElementById("grid-container");
 
+  // Clearing previous grid items if any
+  container.innerHTML = "";
+
   // Setting the grid template columns and rows based on the specified size
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -15,10 +18,32 @@ function createGrid(size) {
     // Adding a class to the grid item for styling purposes
     gridItem.classList.add("grid-item");
 
+    // Adding event listener for 'mouseover' event on each grid item
+    gridItem.addEventListener("mouseover", () => {
+      // Change the background color of the grid item when the mouse hovers over it
+      gridItem.style.backgroundColor = "black"; // The color can be changed as needed
+    });
+
     // Appending the grid item to the container div
     container.appendChild(gridItem);
   }
 }
 
-// Calling the function to create a 16x16 grid
+// Function to change the grid size
+function changeGridSize() {
+  let newSize = prompt("Enter new grid size (max 100):", 16);
+  newSize = parseInt(newSize);
+  if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+    createGrid(newSize);
+  } else {
+    alert("Please enter a valid number between 1 and 100.");
+  }
+}
+
+// Event listener for the 'Change Grid Size' button
+document
+  .getElementById("change-size")
+  .addEventListener("click", changeGridSize);
+
+// Initial call to create a 16x16 grid
 createGrid(16);
